3d3b176e5ddac174aefa1de8dc0c231fcecda8cd
Boris Djordjevic <d.boris@gmail.com>
2026-05-12 02:18:30 +0100

    Remove ritalin archive from repo

--- .ritalin-archive/.ritalin/prove-all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-cd /Users/biobook/Projects/api-key-manager
-for n in 001 002 003 004 005 006 007 008 009 010 011 012 013 014; do
-  out=$(ritalin prove "O-$n" 2>&1)
-  verdict=$(echo "$out" | grep -oE '"verdict": "[a-z]+"' | head -1)
-  printf "O-%s  %s\n" "$n" "$verdict"
-done
